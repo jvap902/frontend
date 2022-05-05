e9e86a436f79f861a284876bffad3efa3e4a79c5
João Vitor Angelo Pereira <jvangelopereira@gmail.com>
2022-05-05 18:45:12 -0300

    progresso com a estruturação do html e js

--- scripts_1.js
+++ scripts_1.js
@@ -1,6 +1,19 @@
+let modal = null
+
 onload = async () => {
+    modal = new bootstrap.Modal(document.getElementById('exampleModal'))
+
     const login = document.getElementById("login")
     login.addEventListener('click', () => {
-        
+      Formulario()  
     })
+}
+const Formulario = (/*id = "", */email = "", senha = "") => {
+    const emailInput = document.getElementById('email')
+    const senhaInput = document.getElementById('senha')
+    //const idInput = document.getElementById('id')
+
+    emailInput.value = email
+    senhaInput.value = senha
+    //idInput.value = id
 }
