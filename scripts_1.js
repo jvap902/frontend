8f02ec44e185fb68b78004e14c054c36ab9fef70
Juanzitozito <juan.cordova@hotmail.com>
2022-05-19 20:20:21 -0300

    progresso botão salvar

--- scripts_1.js
+++ scripts_1.js
@@ -10,24 +10,39 @@ onload = async () => {
     modal_item = new bootstrap.Modal(document.getElementById('div_item'))
     btnAdicionar = document.getElementById("adc_ingrediente")
 
+/*
     const login = document.getElementById("login")
     login.addEventListener('click', () => {
-        Formulario()
+        Formulario() 
       })
 
     const item = document.getElementById("item")
     item.addEventListener('click', () => {
         cadastra_item()
       })
-
-    const btnAdicionar = document.getElementById('adc_ingrediente')
-    btnAdicionar.addEventListener('click', () => {
+*/
+    btnAdicionar.addEventListener('click', async () => {
       const listaIngrediente = document.getElementById('ingrediente').value;
+      const item = document.getElementById('item').value
+
       arrayIngredientes.push(listaIngrediente)
+
+     /* 
+      const body = new FormData()
+      body.append('item', item)
+      body.append('sobrenome', sobrenome)
+
+      const response = await fetch(``){
+        method: "POST",
+        body
+      }
+      const itens = await response.json()
+      */
+
     })
 
 }
-const Formulario = (id = "", nome = "", email = "", senha = "") => {
+const preencheFormulario = (id = "", nome = "", email = "", senha = "") => {
     const nomeInput = document.getElementById('nome')
     const emailInput = document.getElementById('email')
     const senhaInput = document.getElementById('senha')
@@ -39,7 +54,7 @@ const Formulario = (id = "", nome = "", email = "", senha = "") => {
     idInput.value = id
 }
 
-const cadastra_item = (id_item = "", item = "", arrayIngredientes/*??*/) =>{
+const cadastra_item = (id_item = "", item = "", arrayIngredientes = ""/*??*/) =>{
   const itemInput = document.getElementById('item')
   const ingredientesInput = document.getElementById('ingrediente')
   const id_itemInput = document.getElementById('id_item')
@@ -48,9 +63,5 @@ const cadastra_item = (id_item = "", item = "", arrayIngredientes/*??*/) =>{
   ingredientesInput.value = arrayIngredientes //?
   id_itemInput.value = id_item
 
-  console.log(arrayIngredientes)
-
-  
-  
   //segue a mesma logica de antes
 }
