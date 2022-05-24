027686af9072b0b8a273430331a65b8a2a7286cc
Joao Pereira <joao.090405@bento.local>
2022-05-24 09:59:54 -0300

    separação scripts

--- scripts_2.js
+++ scripts_2.js
@@ -1,3 +1,40 @@
+let modal_item = null
+let btnAdicionar = null
+let listaIngrediente = null
+
+const arrayIngredientes = []
+
+onload = () => {
+    modal_item = new bootstrap.Modal(document.getElementById('div_item'))
+    btnSalvarItem = document.getElementById('salvar')
+    btnAdicionarIngrediente = document.getElementById("adc_ingrediente")
+
+    btnAdicionarIngrediente.addEventListener('click', async () => {
+      const input = document.getElementById('ingrediente')
+      
+      arrayIngredientes.push(input.value)
+      input.value = ""
+      
+      const ul = document.getElementById('ingredientes_adc')
+      ul.innerHTML = ""
+      arrayIngredientes.forEach(v => {
+        const li = document.createElement('LI')
+        li.innerHTML = v
+        ul.appendChild(li)
+      })
+
+    })
 
 
-//criar elementos para novas semanas conforme elas são adicionadas
+
+}
+
+const cadastra_item = (id_item = "", item = "", arrayIngredientes = ""/*??*/) =>{
+  const itemInput = document.getElementById('item')
+  const ingredientesInput = document.getElementById('ingrediente')
+  const id_itemInput = document.getElementById('id_item')
+
+  itemInput.value = item
+  ingredientesInput.value = arrayIngredientes //?
+  id_itemInput.value = id_item
+}
