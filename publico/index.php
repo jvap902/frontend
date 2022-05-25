cc5caf8e9bc97894ef052e81edd87cdb9b8840e5
Alyssa Michalski <alyssa.070505@bento.local>
2022-05-25 08:49:38 -0300

    push

--- publico/index.php
+++ publico/index.php
@@ -41,19 +41,35 @@ require_once ("../pw3-cardapio_ru-backend/permissao.php")
 
 
   <div id="tabelas">
-    <table>
-      <thead>
-        <tr>
-          <th scope="col">Semana</th>
-        </tr>
-        <tr>
-          <th>Data</th>
-        </tr>
-      </thead>
-      <tbody>
-  
-      </tbody>
-    </table>
+  <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
   </div>
   
 
