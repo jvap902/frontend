458461d342e4073d2c573bd18d8b700c2b298490
Alyssa Michalski <alyssa.070505@bento.local>
2022-05-31 09:46:36 -0300

    ids

--- publico/index.php
+++ publico/index.php
@@ -38,41 +38,41 @@
     <table class="table table-bordered table-hover">
       <thead>
         <tr>
-          <th colspan="6" class="text-center">Semana xxx</th>
+          <th colspan="6" class="text-center" id="semana">Semana xxx</th>
         </tr>
       </thead>
       <tbody>
       <tr>
           <th ></th>
-          <th class="text-center">Dia xx/xx/xxxx</th>
-          <th class="text-center">Dia xx/xx/xxxx</th>
-          <th class="text-center">Dia xx/xx/xxxx</th>
-          <th class="text-center">Dia xx/xx/xxxx</th>
-          <th class="text-center">Dia xx/xx/xxxx</th>
+          <th class="text-center" id="thSegunda">Dia xx/xx/xxxx</th>
+          <th class="text-center" id="thTerca">Dia xx/xx/xxxx</th>
+          <th class="text-center" id="thQuarta">Dia xx/xx/xxxx</th>
+          <th class="text-center" id="thQuinta">Dia xx/xx/xxxx</th>
+          <th class="text-center" id="thSexta">Dia xx/xx/xxxx</th>
         </tr>
         <tr>
-          <th scope="row">Café da manhã</th>
-          <td>Pratos, pratos, pratos</td>
-          <td>Pratos, pratos, pratos</td>
-          <td>Pratos, pratos, pratos</td>
-          <td>Pratos, pratos, pratos</td>
-          <td>Pratos, pratos, pratos</td>
+          <th scope="row" id="thCafe">Café da manhã</th>
+          <td id="cafeSegunda">Pratos, pratos, pratos</td>
+          <td id="cafeTerca">Pratos, pratos, pratos</td>
+          <td id="cafeQuarta">Pratos, pratos, pratos</td>
+          <td id="cafeQuinta">Pratos, pratos, pratos</td>
+          <td id="cafeSexta">Pratos, pratos, pratos</td>
         </tr>
         <tr>
-          <th scope="row">Almoço</th>
-          <td>Pratos, pratos, pratos</td>
-          <td>Pratos, pratos, pratos</td>
-          <td>Pratos, pratos, pratos</td>
-          <td>Pratos, pratos, pratos</td>
-          <td>Pratos, pratos, pratos</td>
+          <th scope="row" id="thAlmoco">Almoço</th>
+          <td id="almocoSegunda">Pratos, pratos, pratos</td>
+          <td id="almocoTerca">Pratos, pratos, pratos</td>
+          <td id="almocoQuarta">Pratos, pratos, pratos</td>
+          <td id="almocoQuinta">Pratos, pratos, pratos</td>
+          <td id="almocoSexta">Pratos, pratos, pratos</td>
         </tr>
         <tr>
-          <th scope="row">Janta</th>
-          <td>Pratos, pratos, pratos</td>
-          <td>Pratos, pratos, pratos</td>
-          <td>Pratos, pratos, pratos</td>
-          <td>Pratos, pratos, pratos</td>
-          <td>Pratos, pratos, pratos</td>
+          <th scope="row" id="thJanta">Janta</th>
+          <td id="jantaSegunda">Pratos, pratos, pratos</td>
+          <td id="jantaTerca">Pratos, pratos, pratos</td>
+          <td id="jantaQuarta">Pratos, pratos, pratos</td>
+          <td id="jantaQuinta">Pratos, pratos, pratos</td>
+          <td id="jantaSexta">Pratos, pratos, pratos</td>
         </tr>
       </tbody>
     </table>
